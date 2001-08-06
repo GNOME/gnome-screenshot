02e850163dce4cc88cbbf84df232cfbb946819e8
Martin Baulig <baulig@suse.de>
2001-08-06 21:14:42 +0000

    #include <libgnomeui.h> instead of <gnome.h> and #include
    2001-08-06  Martin Baulig  <baulig@suse.de>
    
    	* *.[ch]: #include <libgnomeui.h> instead of <gnome.h> and
    	#include <bonobo-activation/bonobo-activation.h> instead
    	of <libgnorba/gnorba.h>.

--- gnome-panel-screenshot.c
+++ gnome-panel-screenshot.c
@@ -24,7 +24,7 @@
 /* IF YOU WANT YOUR OWN FEATURE -- WRITE THE DAMN THING YOURSELF (-:*/
 
 #include <config.h>
-#include <gnome.h>
+#include <libgnomeui.h>
 #include <glade/glade.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gdk/gdkx.h>
