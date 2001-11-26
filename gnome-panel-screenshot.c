328e895d29b27f23f5e17e99d4c17d6cefbd7519
Gediminas Paulauskas <menesis@delfi.lt>
2001-11-26 23:36:05 +0000

    always get translated messages in UTF-8.
    2001-11-27  Gediminas Paulauskas <menesis@delfi.lt>
    
    	* all files containing main(): always get translated messages in
    	UTF-8.

--- gnome-panel-screenshot.c
+++ gnome-panel-screenshot.c
@@ -1020,6 +1020,7 @@ main (int argc, char *argv[])
 
 	setlocale (LC_ALL, "");
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
 	gnome_init_with_popt_table ("gnome-panel-screenshot", VERSION,
