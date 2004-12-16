9cdb2da350043dac151afc432ee5a7b6ac631672
Vincent Noel <vnoel@cox.net>
2004-12-16 15:48:31 +0000

    Use new GtkFileChooserButton API.
    2004-12-16  Vincent Noel  <vnoel@cox.net>
    
    	* screenshot-dialog.c (screenshot_dialog_new): Use new
    	GtkFileChooserButton API.

--- screenshot-dialog.c
+++ screenshot-dialog.c
@@ -210,7 +210,7 @@ screenshot_dialog_new (GdkPixbuf *screenshot,
   dialog->filename_entry = glade_xml_get_widget (dialog->xml, "filename_entry");
   file_chooser_frame = glade_xml_get_widget (dialog->xml, "file_chooser_frame");
 
-  dialog->save_widget = gtk_file_chooser_button_new ("Select a directory");
+  dialog->save_widget = gtk_file_chooser_button_new ("Select a directory", GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
   gtk_file_chooser_set_local_only (GTK_FILE_CHOOSER (dialog->save_widget), FALSE);
   gtk_file_chooser_set_action (GTK_FILE_CHOOSER (dialog->save_widget),
 			       GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
