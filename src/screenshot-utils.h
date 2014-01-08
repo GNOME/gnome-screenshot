ae191d21863d5c8d420a5d83199d3fd903341e67
Dominique Leuenberger <dimstar@opensuse.org>
2014-01-08 22:11:15 +0100

    Update FSF Address
    https://bugzilla.gnome.org/show_bug.cgi?id=721503

--- src/screenshot-utils.h
+++ src/screenshot-utils.h
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  */
 
 #ifndef __SCREENSHOT_UTILS_H__
