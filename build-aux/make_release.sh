98eb1b9d5363862c0961c2eea8a67488b7ca52f3
Emmanuele Bassi <ebassi@gnome.org>
2017-05-29 13:18:46 +0100

    Add Meson build
    Meson is a meta-build system that has several advantages over the
    existing Autotools build:
    
      - it's faster
      - it's easier to understand
      - it's well maintained
      - it's well integrated with the GNOME tooling
    
    Various projects in the GNOME ecosystem are moving away from Autootols
    and using Meson as a replacement.

---
+++ build-aux/make_release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -x
+
+test -n "$srcdir" || srcdir=$1
+test -n "$srcdir" || srcdir=.
+
+cd "$srcdir"
+
+PROJECT=gnome-screenshot
+VERSION=$(git describe --abbrev=0)
+NAME="${PROJECT}-${VERSION}"
+
+rm -f "${NAME}.tar"
+
+echo "Creating git tree archive…"
+git archive --prefix="${NAME}/" --format=tar HEAD > ${NAME}.tar
+
+rm -f "${NAME}.tar.xz"
+
+echo "Compressing archive…"
+xz -f "${NAME}.tar"
+
+set +x
