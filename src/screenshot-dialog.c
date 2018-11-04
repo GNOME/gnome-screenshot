fbfc828b634b9cae3a1f9ecc4abcc2f8754601a7
Cosimo Cecchi <cosimoc@gnome.org>
2018-11-04 14:56:20 -0800

    dialog: call right function to get display
    This is a typo that was introduced in commit
    b6d17c508c832d9131c60e85eb50c23a909e85cf.

--- src/screenshot-dialog.c
+++ src/screenshot-dialog.c
@@ -335,7 +335,7 @@ screenshot_dialog_set_busy (ScreenshotDialog *dialog,
       GdkDisplay *display;
       GdkCursor *cursor;
       /* Change cursor to busy */
-      display = gtk_widget_get_window (GTK_WIDGET (dialog));
+      display = gtk_widget_get_display (GTK_WIDGET (dialog));
       cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
       gdk_window_set_cursor (window, cursor);
       g_object_unref (cursor);
