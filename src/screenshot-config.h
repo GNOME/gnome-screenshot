290ecfef391170d749350a9e111f4fa18bd1e84a
Christopher Davis <brainblasted@disroot.org>
2018-12-08 17:33:28 -0500

    Redesign Screenshot's main window
    The dialog-style window of Screenshot introduced some
    complications when it came to designing for the app menu
    initiative: https://gitlab.gnome.org/GNOME/Initiatives/issues/4
    
    The window was redesigned by Tobias Bernard with new icons from
    Jakub Steiner. The window effects were simplified down to a drop
    shadow.
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-screenshot/issues/15

--- src/screenshot-config.h
+++ src/screenshot-config.h
@@ -41,7 +41,8 @@ typedef struct {
   gboolean include_icc_profile;
 
   gboolean include_border;
-  gchar *border_effect;
+
+  gboolean use_shadow;
 
   guint delay;
 
@@ -58,7 +59,7 @@ gboolean    screenshot_config_parse_command_line  (gboolean clipboard_arg,
                                                    gboolean include_border_arg,
                                                    gboolean disable_border_arg,
                                                    gboolean include_pointer_arg,
-                                                   const gchar *border_effect_arg,
+                                                   gboolean use_shadow_arg,
                                                    guint delay_arg,
                                                    gboolean interactive_arg,
                                                    const gchar *file_arg);
